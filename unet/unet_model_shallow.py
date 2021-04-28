7ec21210c9aa064e880a66a0d1ebc13f87108758
ceynri <ceynri@gmail.com>
2021-04-28 22:50:22 +0800

    refactor: 调整网络上采样层的命名

--- unet/unet_model_shallow.py
+++ unet/unet_model_shallow.py
@@ -17,9 +17,9 @@ class UNet(nn.Module):
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512 // factor)
-        self.up1 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(512, 256 // factor, bilinear)
         self.up2 = Up(256, 128 // factor, bilinear)
-        self.up3 = Up(128, 64, bilinear)
+        self.up1 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
@@ -27,8 +27,8 @@ class UNet(nn.Module):
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x = self.up1(x4, x3)
+        x = self.up3(x4, x3)
         x = self.up2(x, x2)
-        x = self.up3(x, x1)
+        x = self.up1(x, x1)
         logits = self.outc(x)
         return logits
